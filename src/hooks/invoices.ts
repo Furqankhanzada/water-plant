1bf1866fb29104fc1d8c2e0642f6689d86bdf6a0
Muhammad Furqan <furqan.khanzada@gmail.com>
2025-01-05 22:30:19 +0500

    chore: add hook for manage transactions status based on invoice status

--- src/hooks/invoices.ts
+++ src/hooks/invoices.ts
@@ -1,40 +1,25 @@
-import type { CollectionAfterChangeHook } from 'payload'
+import type { CollectionAfterOperationHook } from 'payload'
+
 import { Invoice } from '@/payload-types'
 
-export const afterChangeHook: CollectionAfterChangeHook = async ({
-  doc,
+export const afterOperationHook: CollectionAfterOperationHook = async ({
+  result,
   operation,
   req: { payload },
 }) => {
-  const invoiceResult = doc as Invoice
-
-  console.log('Operation:', operation)
-  console.log('Invoice transaction IDs:', invoiceResult.transaction)
-
-  if (
-    (operation === 'create' || operation === 'update') &&
-    Array.isArray(invoiceResult.transaction) &&
-    invoiceResult.transaction.length > 0
-  ) {
-    try {
-      await payload.update({
-        collection: 'transaction',
-        where: {
-          id: {
-            in: invoiceResult.transaction,
-          },
+  const invoiceResult = result as Invoice
+  if (operation === 'create' || operation === 'updateByID') {
+    await payload.update({
+      collection: 'transaction',
+      where: {
+        id: {
+          in: invoiceResult.transactions,
         },
-        data: {
-          status: 'pending',
-        },
-      })
-      console.log('Transaction status updated to pending for IDs:', invoiceResult.transaction)
-    } catch (error) {
-      console.error('Error updating transaction status:', error)
-    }
-  } else {
-    console.warn('No valid transaction IDs found for invoice:', invoiceResult)
+      },
+      data: {
+        status: invoiceResult.status === 'unpaid' ? 'pending' : 'paid',
+      },
+    })
   }
-
-  return doc
+  return result
 }
