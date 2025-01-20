0082b5e5fbc4cb14dd80e43d45289c827a1afc4e
Muhammad Furqan <furqan.khanzada@gmail.com>
2025-01-20 14:03:35 +0500

    chore: only run on update

--- src/hooks/invoices/changeTransactionsStatusOnRemoval.ts
+++ src/hooks/invoices/changeTransactionsStatusOnRemoval.ts
@@ -2,9 +2,12 @@ import type { CollectionBeforeChangeHook } from 'payload'
 
 export const changeTransactionsStatusOnRemoval: CollectionBeforeChangeHook = async ({
   req: { payload },
+  operation,
   data,
   originalDoc,
 }) => {
+  if (operation !== 'update') return data
+
   if (originalDoc.transactions.length) {
     const deletedTransactions = originalDoc.transactions.filter(
       (otId: string) => !data.transactions.includes(otId),
