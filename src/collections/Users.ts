93f50b79e3338d24f3cc65395a25bfc454301206
Muhammad Furqan <furqan.khanzada@gmail.com>
2025-01-31 17:02:44 +0500

    init: role based permissions

--- src/collections/Users.ts
+++ src/collections/Users.ts
@@ -1,13 +1,40 @@
 import type { CollectionConfig } from 'payload'
 
+import { isAdmin, isAdminFieldLevel } from './access/isAdmin'
+import { isAdminOrSelf } from './access/isAdminOrSelf'
+
 export const Users: CollectionConfig = {
   slug: 'users',
   admin: {
     useAsTitle: 'email',
   },
+  access: {
+    create: isAdmin,
+    read: isAdminOrSelf,
+    update: isAdminOrSelf,
+    delete: isAdmin,
+  },
   auth: true,
   fields: [
-    // Email added by default
-    // Add more fields as needed
+    {
+      name: 'fullName',
+      type: 'text',
+    },
+    {
+      name: 'roles',
+      type: 'select',
+      saveToJWT: true,
+      hasMany: true,
+      required: true,
+      access: {
+        create: isAdminFieldLevel,
+        update: isAdminFieldLevel,
+      },
+      options: [
+        { label: 'Admin', value: 'admin' },
+        { label: 'Editor', value: 'editor' },
+      ],
+      defaultValue: 'editor',
+    },
   ],
 }
