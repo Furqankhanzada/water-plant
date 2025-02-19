e1da6b27206035b81243a1d3b7e20b2ca329e406
Muhammad Furqan <furqan.khanzada@gmail.com>
2025-02-19 17:15:49 +0500

    chore: branding (#61)

--- src/app/(payload)/admin/importMap.js
+++ src/app/(payload)/admin/importMap.js
@@ -2,10 +2,14 @@ import { ContactNumberCell as ContactNumberCell_73182a10d002044f26d480e9c0a389e0
 import { GeneratePdfButton as GeneratePdfButton_91735f33071acecbd7cb388640edace0 } from 'src/components/Trips'
 import { Info as Info_91735f33071acecbd7cb388640edace0 } from 'src/components/Trips'
 import { GeneratePdfButton as GeneratePdfButton_70a30823add7ef7114b7676ba5a6f720 } from 'src/components/Invoices'
+import { Icon as Icon_f9f58eabab541759fc9306399f740359 } from 'src/graphics/Branding.tsx'
+import { Logo as Logo_f9f58eabab541759fc9306399f740359 } from 'src/graphics/Branding.tsx'
 
 export const importMap = {
   "/components/Customers#ContactNumberCell": ContactNumberCell_73182a10d002044f26d480e9c0a389e0,
   "/components/Trips#GeneratePdfButton": GeneratePdfButton_91735f33071acecbd7cb388640edace0,
   "/components/Trips#Info": Info_91735f33071acecbd7cb388640edace0,
-  "/components/Invoices#GeneratePdfButton": GeneratePdfButton_70a30823add7ef7114b7676ba5a6f720
+  "/components/Invoices#GeneratePdfButton": GeneratePdfButton_70a30823add7ef7114b7676ba5a6f720,
+  "/graphics/Branding.tsx#Icon": Icon_f9f58eabab541759fc9306399f740359,
+  "/graphics/Branding.tsx#Logo": Logo_f9f58eabab541759fc9306399f740359
 }
---
+++ src/graphics/Branding.tsx
@@ -0,0 +1,39 @@
+export const Icon = async () => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      xmlSpace="preserve"
+      width={18}
+      height={18}
+      viewBox="0 0 25.906 25.906"
+    >
+      <path
+        d="M12.953 0s-9 10.906-9 16.906c0 4.971 4.029 9 9 9s9-4.029 9-9c0-6-9-16.906-9-16.906zM9.026 17.496c0 1.426.668 4.25 1.134 5.426-3.042-1.494-3.846-4.425-3.846-6.463 0-3.173 3.684-7.824 5.777-12.149-.23 2.271-3.065 8.867-3.065 13.186z"
+        style={{
+          fill: '#030104',
+        }}
+      />
+    </svg>
+  )
+}
+export const Logo = async () => {
+  return (
+    <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        xmlSpace="preserve"
+        width={100}
+        height={100}
+        viewBox="0 0 25.906 25.906"
+      >
+        <path
+          d="M12.953 0s-9 10.906-9 16.906c0 4.971 4.029 9 9 9s9-4.029 9-9c0-6-9-16.906-9-16.906zM9.026 17.496c0 1.426.668 4.25 1.134 5.426-3.042-1.494-3.846-4.425-3.846-6.463 0-3.173 3.684-7.824 5.777-12.149-.23 2.271-3.065 8.867-3.065 13.186z"
+          style={{
+            fill: '#030104',
+          }}
+        />
+      </svg>
+      <h1 style={{ fontSize: '40px', lineHeight: '40px' }}>SuqyaTech</h1>
+    </div>
+  )
+}
