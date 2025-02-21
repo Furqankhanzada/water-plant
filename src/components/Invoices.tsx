36937ae2cef11d50f9dff7cfab1ceb266e958796
Muhammad Furqan <furqan.khanzada@gmail.com>
2025-02-21 17:17:29 +0500

    chore: send invoice button

--- src/components/Invoices.tsx
+++ src/components/Invoices.tsx
@@ -1,6 +1,8 @@
 import Link from 'next/link'
 import { ServerComponentProps } from 'payload'
 
+import { isWhatsAppEnabled } from '@/lib/sendWhatsAppMessage'
+
 export const GeneratePdfButton = async (
   props: ServerComponentProps & { rowData: { id: string } },
 ) => {
@@ -18,6 +20,30 @@ export const GeneratePdfButton = async (
       className={`btn btn--size-${props.id ? 'medium' : 'small'} btn--style-primary`}
     >
       Generate Invoice
+    </Link>
+  )
+}
+
+export const SendInvoiceButton = async (
+  props: ServerComponentProps & { rowData: { id: string } },
+) => {
+  if (!isWhatsAppEnabled()) return null
+  if (!props.id && !props.rowData) {
+    return null
+  }
+  const invoiceId = props.id ? props.id : props.rowData.id
+  return (
+    <Link
+      target="_blank"
+      style={{
+        display: 'inline-block',
+        marginBlock: `${props.id ? 'revert-layer' : 'auto'}`,
+        ...(props.id && { marginLeft: '10px' }),
+      }}
+      href={`/invoices/${invoiceId}/send`}
+      className={`btn btn--size-${props.id ? 'medium' : 'small'} btn--style-pill`}
+    >
+      Send Invoice
     </Link>
   )
 }
