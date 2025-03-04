a484b4876da12239d8a96cc4a80f421a3a920536
Muhammad Furqan <furqan.khanzada@gmail.com>
2025-03-04 07:52:55 +0500

    chore: add api key support

--- src/payload-types.ts
+++ src/payload-types.ts
@@ -115,6 +115,9 @@ export interface User {
   roles: ('admin' | 'editor')[];
   updatedAt: string;
   createdAt: string;
+  enableAPIKey?: boolean | null;
+  apiKey?: string | null;
+  apiKeyIndex?: string | null;
   email: string;
   resetPasswordToken?: string | null;
   resetPasswordExpiration?: string | null;
@@ -603,6 +606,9 @@ export interface UsersSelect<T extends boolean = true> {
   roles?: T;
   updatedAt?: T;
   createdAt?: T;
+  enableAPIKey?: T;
+  apiKey?: T;
+  apiKeyIndex?: T;
   email?: T;
   resetPasswordToken?: T;
   resetPasswordExpiration?: T;
