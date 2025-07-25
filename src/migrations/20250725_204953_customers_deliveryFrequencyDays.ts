87cfa420b65b9cd795b24f78dfece75b6494a26c
shaheem-khanzada <shaheem340@gmail.com>
2025-07-26 04:39:00 +0500

    refactor: unset deliveryFrequencyDays to empty string

--- src/migrations/20250725_204953_customers_deliveryFrequencyDays.ts
+++ src/migrations/20250725_204953_customers_deliveryFrequencyDays.ts
@@ -25,7 +25,7 @@ export async function down({ payload }: MigrateDownArgs): Promise<void> {
     },
     {
       $unset: {
-        deliveryFrequencyDays: 4,
+        deliveryFrequencyDays: '',
       },
     },
   )
