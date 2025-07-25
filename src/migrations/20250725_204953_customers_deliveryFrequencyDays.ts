8f27bd40bdda25e47e30344fdbb43be940d77d3d
shaheem-khanzada <shaheem340@gmail.com>
2025-07-26 04:16:05 +0500

    chore: added migration for customers

---
+++ src/migrations/20250725_204953_customers_deliveryFrequencyDays.ts
@@ -0,0 +1,32 @@
+import {
+  MigrateDownArgs,
+  MigrateUpArgs,
+} from '@payloadcms/db-mongodb'
+
+export async function up({ payload }: MigrateUpArgs): Promise<void> {
+  // Migration code
+  await payload.db.collections.customers.collection.updateMany(
+    {
+      deliveryFrequencyDays: { $exists: false },
+    },
+    {
+      $set: {
+        deliveryFrequencyDays: 4,
+      },
+    },
+  )
+}
+
+export async function down({ payload }: MigrateDownArgs): Promise<void> {
+  // Migration code
+  await payload.db.collections.customers.collection.updateMany(
+    {
+      deliveryFrequencyDays: { $exists: true },
+    },
+    {
+      $unset: {
+        deliveryFrequencyDays: 4,
+      },
+    },
+  )
+}
