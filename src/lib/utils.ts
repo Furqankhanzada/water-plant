573e6ce69760ef2c8438a4cc5fd1f37002ded742
shaheem-khanzada <shaheem340@gmail.com>
2025-07-26 14:56:41 +0500

    chore: removed normalizeIds function

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -17,24 +17,4 @@ export function formatDistanceWithFallback(
   }
 
   return formatDistance(new Date(date), now, { addSuffix: true, ...otherOptions });
-}
-
-export function normalizeIds(obj: any): any {
-  if (Array.isArray(obj)) {
-    return obj.map(normalizeIds);
-  } else if (obj && typeof obj === 'object') {
-    const newObj: any = {};
-    for (const key in obj) {
-      const val = obj[key];
-
-      // Convert ObjectId to string
-      if (Types.ObjectId.isValid(val) && String(val) === val.toString()) {
-        newObj[key === '_id' ? 'id' : key] = isNaN(val) ? val.toString() : val;
-      } else {
-        newObj[key === '_id' ? 'id' : key] = isValid(val) ? val : normalizeIds(val);
-      }
-    }
-    return newObj;
-  }
-  return obj;
-}
+};
