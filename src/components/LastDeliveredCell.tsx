a3ffba797df7904c69e285718e999a4ce98ba36c
shaheem-khanzada <shaheem340@gmail.com>
2025-07-29 14:35:45 +0500

    chore: updated LastDeliveredCell to use updatedAt instead of transactionAt date

--- src/components/LastDeliveredCell.tsx
+++ src/components/LastDeliveredCell.tsx
@@ -1,30 +1,32 @@
 import { DefaultServerCellComponentProps } from 'payload'
 import { formatDistanceWithFallback } from '@/lib/utils'
 
-const LastDeliveredCell = async ({ rowData, payload, collectionSlug }: DefaultServerCellComponentProps) => {
-  const customerId = collectionSlug === 'customers' ? rowData.id : rowData.customer;
-  const lastTransactionAt = (await payload.find({
-    collection: 'transaction',
-    where: {
-      customer: {
-        equals: customerId,
+const LastDeliveredCell = async ({
+  rowData,
+  payload,
+  collectionSlug,
+}: DefaultServerCellComponentProps) => {
+  const customerId = collectionSlug === 'customers' ? rowData.id : rowData.customer
+  const lastTransactionAt = (
+    await payload.find({
+      collection: 'transaction',
+      where: {
+        customer: {
+          equals: customerId,
+        },
+        bottleGiven: {
+          greater_than: 0,
+        },
       },
-      bottleGiven: {
-        greater_than: 0,
-      }
-    },
-    limit: 1,
-    sort: '-transactionAt',
-    select: {
-      transactionAt: true,
-    }
-  })).docs[0]?.transactionAt;
+      limit: 1,
+      sort: '-transactionAt',
+      select: {
+        updatedAt: true,
+      },
+    })
+  ).docs[0]?.updatedAt
 
-  return (
-    <div>
-      {formatDistanceWithFallback(lastTransactionAt, { fallback: 'Never Delivered' })}
-    </div>
-  )
-};
+  return <div>{formatDistanceWithFallback(lastTransactionAt, { fallback: 'Never Delivered' })}</div>
+}
 
-export default LastDeliveredCell;
+export default LastDeliveredCell
