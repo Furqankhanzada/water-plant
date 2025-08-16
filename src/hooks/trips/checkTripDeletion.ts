e95636cfba0c83aa401b42c8d6b87bd6b889f3e9
Muhammad Furqan <furqan.khanzada@gmail.com>
2025-08-16 11:57:50 +0500

    chore: trip safe deletion

---
+++ src/hooks/trips/checkTripDeletion.ts
@@ -0,0 +1,17 @@
+import { BeforeDeleteHook } from 'node_modules/payload/dist/collections/config/types';
+import { APIError } from 'payload'
+
+export const checkTripDeletion: BeforeDeleteHook = async ({ req, id }) => {
+  // Check for related transactions
+  const transactions = await req.payload.find({
+    collection: 'transaction',
+    where: {
+      trip: { equals: id }
+    },
+    limit: 1
+  });
+
+  if (transactions.docs.length > 0) {
+    throw new APIError('Cannot delete trip: Trip has associated transactions. Please delete all transactions first.', 400);
+  }
+}
