eb3f9d6c3a99da5d5c0084c397f8e1f254b4d92d
Muhammad Furqan <furqan.khanzada@gmail.com>
2025-08-17 10:11:13 +0500

    chore: during deletion do not check for trip

--- src/hooks/transactions/checkTransactionDeletion.ts
+++ src/hooks/transactions/checkTransactionDeletion.ts
@@ -14,36 +14,7 @@ export const checkTransactionDeletion: CollectionBeforeDeleteHook = async ({ req
     },
   });
 
-  if (invoices.docs.length > 0) {
+  if (invoices.totalDocs) {
     throw new APIError('Cannot delete transaction: Transaction is associated with an invoice. Please remove it from the invoice first.', 400);
-  }
-
-  // Check if the transaction is part of an active trip
-  const transaction = await req.payload.findByID({
-    collection: 'transaction',
-    id,
-    depth: 0,
-    select: {
-      trip: true,
-    },
-  });
-
-  if (!transaction) {
-    throw new APIError('Transaction not found', 404);
-  }
-
-  if (transaction.trip) {
-    const trip = await req.payload.findByID({
-      collection: 'trips',
-      id: transaction.trip as string,
-      depth: 0,
-      select: {
-        status: true,
-      },
-    });
-
-    if (trip && trip.status === 'inprogress') {
-      throw new APIError('Cannot delete transaction: Transaction is part of an active trip. Please complete or cancel the trip first.', 400);
-    }
   }
 }
