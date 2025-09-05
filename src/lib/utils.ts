e93148c47bc28634c267a6b4bf7c17df546de85d
Muhammad Furqan <furqan.khanzada@gmail.com>
2025-09-05 21:22:51 +0500

    chore: add missing function

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,25 @@
+import { isValid, formatDistance, FormatDistanceOptions } from 'date-fns'
 import { clsx, type ClassValue } from "clsx"
 import { twMerge } from "tailwind-merge"
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+type FormatDistanceWithFallbackOptions = FormatDistanceOptions & {
+  fallback: string;
+};
+
+export function formatDistanceWithFallback(
+  date: Date | string | undefined,
+  options: FormatDistanceWithFallbackOptions
+): string {
+  const { fallback, ...otherOptions } = options;
+  const now = new Date();
+
+  if (!date || !isValid(new Date(date))) {
+    return fallback;
+  }
+
+  return formatDistance(new Date(date), now, { addSuffix: true, ...otherOptions });
+};
