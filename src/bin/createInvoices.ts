0f72aaa5a8ac0f6dc1c0792e9e03c031bd72f206
shaheem-khanzada <shaheem340@gmail.com>
2025-12-01 04:02:51 +0500

    chore: fixed invoice send issues

--- src/bin/createInvoices.ts
+++ src/bin/createInvoices.ts
@@ -1,4 +1,4 @@
-import { generateAndSendInvoices } from '@/scripts/generateAndSendInvoices'
+import { generateInvoices } from '@/scripts/generateInvoices'
 import type { SanitizedConfig } from 'payload'
 
 import payload from 'payload'
@@ -6,7 +6,7 @@ import payload from 'payload'
 // Script must define a "script" function export that accepts the sanitized config
 export const script = async (config: SanitizedConfig) => {
   await payload.init({ config })
-  await generateAndSendInvoices(payload)
+  await generateInvoices(payload)
   payload.logger.info('Successfully created and sent invoices!')
   process.exit(0)
 }
